4d1020bcc1c509dcba93849d7624322fdca486cc
dlcllego-hue <dlcllego@cmc.pshs.edu.ph>
2026-04-20 10:44:56 +0800

    Update FinalGradeCalculator.py

--- FinalGradeCalculator.py
+++ FinalGradeCalculator.py
@@ -1,5 +1,6 @@
-third_quarter_grade = #
-fourth_quarter_grade = #
+third_quarter_grade = input("Enter the third quarter grade: ")
+fourth_quarter_grade = input("Enter the fourth quarter grade: ")
+
 final_grade_third = float(third_quarter_grade) / 3
 final_grade_fourth = float(fourth_quarter_grade) / 3 * 2
 final_grade = final_grade_third + final_grade_fourth
